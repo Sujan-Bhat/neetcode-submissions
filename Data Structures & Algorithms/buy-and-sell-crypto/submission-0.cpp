c3e1ce14550406ec831cc44f98f3ef9638362503
Sujan Bhat <155548913+Sujan-Bhat@users.noreply.github.com>
2026-08-26 11:05:23 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        //brute force
+        for(int i = 0; i < prices.size()-1; i++){
+            
+            for(int j = i+1; j < prices.size(); j++){
+                if((prices[j] - prices[i]) > profit){
+                    profit = prices[j] - prices[i];
+                }
+            }
+
+        }
+        return profit;
+    }
+};
